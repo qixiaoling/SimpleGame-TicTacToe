7ba114de2fdecacf526bbaa6f8419a73fbe7115e
qixiaoling <x.qi@novi-education.nl>
2022-05-17 10:02:07 +0200

    third commit

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,4 +1,4 @@
-package TicTacToe_Xiaoling;
+package TicTacToe;
 
 
 import java.util.Random;
@@ -23,15 +23,32 @@ public class TicTacToe {
             printBoard(board);
         }
 
-        //scanner.close();
-        //printBoard(board);
+        scanner.close();
     }
 
     private static boolean isGameFinished(char[][] board) {
 
+        if(hasContestantWon(board, 'X')){
+            printBoard(board);
+            System.out.println("Player wins!");
+            return true;
+        }
+        if(hasContestantWon(board, 'O')){
+            printBoard(board);
+            System.out.println("Computer wins!");
+            return true;
+        }
 
-
-        return false;
+        for (int i= 0; i<board.length; i++){
+            for (int j = 0; j<board[i].length; j++){
+                if(board[i][j] == ' '){
+                    return false;
+                }
+            }
+        }
+        printBoard(board);
+        System.out.println("The game ended in a tie!");
+        return true;
     }
 
     private static void computerTurn(char[][] board) {
@@ -40,7 +57,7 @@ public class TicTacToe {
         int computerMove;
         while(true){
             computerMove = rand.nextInt(9) + 1;
-            if(isValidMove(board, computerMove)){
+            if(isValidMove(board, Integer.toString(computerMove))){
                 break;
             }
         }
@@ -53,7 +70,7 @@ public class TicTacToe {
         while(true){
             System.out.println("Where would you like to play? (1-9)");
             userInput = scanner.nextLine();
-            if(isValidMove(board, Integer.parseInt(userInput))){
+            if(isValidMove(board, userInput)){
                 break;
             }else{
                 System.out.println(userInput + " is not a valid move");
@@ -62,31 +79,47 @@ public class TicTacToe {
         placeMove(board, userInput, 'X' );
     }
 
-    private static boolean isValidMove(char[][] board, int position) {
+    private static boolean isValidMove(char[][] board, String position) {
 
         switch (position) {
-            case 1:
+            case "1":
                 return (board[0][0] == ' '); //same effect as "if-else", it returns the value of this expression
-            case 2:
+            case "2":
                 return (board[0][1] == ' ');
-            case 3:
+            case "3":
                 return (board[0][2] == ' ');
-            case 4:
+            case "4":
                 return (board[1][0] == ' ');
-            case 5:
+            case "5":
                 return (board[1][1] == ' ');
-            case 6:
+            case "6":
                 return (board[1][2] == ' ');
-            case 7:
+            case "7":
                 return (board[2][0] == ' ');
-            case 8:
+            case "8":
                 return (board[2][1] == ' ');
-            case 9:
+            case "9":
                 return (board[2][2] == ' ');
             default:
                 return false;
 
         }
+    }
+
+    private static boolean hasContestantWon(char[][] board, char symbol) {
+        if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
+                (board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
+                (board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+
+                (board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
+                (board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
+                (board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+
+                (board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
+                (board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+            return true;
+        }
+        return false;
     }
 
     private static void placeMove(char[][] board, String position, char symbol) {
